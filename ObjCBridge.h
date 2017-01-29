33a6f123c5a3c7358aef7b2623147e9f7a33c4b0
mpatel16 <Mayur@mayurs-macbook-pro.local>
2017-01-28 21:22:06 -0500

    SDKEstimote

---
+++ ObjCBridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjCBridge.h
+//  CrowdsV2
+//
+//  Created by Mayur Patel on 1/28/17.
+//  Copyright © 2017 Gaurang Bham. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
